ca57bc527ad1c922316017be803ebb2434012c81
Michael Freese <mrfreese1@gmail.com>
2016-10-10 15:09:14 -0400

    changes

--- coffee.rb
+++ coffee.rb
@@ -1,3 +1,14 @@
 class Coffee
-  
+  attr_accessor :type,
+                :amount
+  def initialize(type, amount = 1)
+    self.type = type
+    self.amount = amount
+  end
+  def full?
+    self.amount == 1
+  end
+  def empty?
+    current_amount == 0
+  end
 end
--- human.rb
+++ human.rb
@@ -1,11 +1,24 @@
 class Human
   attr_accessor :name,
-                :alertness
+                :alertness,
+                :coffee
+
 
   def initialize(name, alertness=0)
     self.name = name
     self.alertness = alertness
   end
-
+    def has_coffee?
+    self.coffee.to_i > 0
+  end
+  def needs_coffee?
+    self.coffee.to_i == 0
+  end
+  def buy (x)
+    self.coffee = x
+  end
+  def drink!
+    self.drink = 
+  end
 
 end
