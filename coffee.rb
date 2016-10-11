a92f772cb2428674b75dbe2f2266ad92077c3376
Michael Freese <mrfreese1@gmail.com>
2016-10-10 21:55:46 -0400

    submit

--- coffee.rb
+++ coffee.rb
@@ -1,18 +1,20 @@
 class Coffee
-  attr_accessor :type,
-                :amount
-  def initialize(type, amount = 1)
-    self.type = type
-    self.amount = amount
-  end
-  def full?
-    self.amount == 1
-  end
-  def empty?
-    self.amount <= 0.01
-  end
-  def sip!
-    self.amount = self.amount - 0.33
-  end
+    attr_accessor :type,
+                  :amount
+    def initialize(type, amount = 1)
+        self.type = type
+        self.amount = amount
+    end
 
+    def full?
+        amount == 1
+    end
+
+    def empty?
+        amount <= 0.01
+    end
+
+    def sip!
+        self.amount = amount - 0.33
+    end
 end
--- human.rb
+++ human.rb
@@ -1,27 +1,27 @@
 class Human
-  attr_accessor :name,
-                :alertness,
-                :coffee
+    attr_accessor :name,
+                  :alertness,
+                  :coffee
 
+    def initialize(name, alertness = 0)
+        self.name = name
+        self.alertness = alertness
+    end
 
-  def initialize(name, alertness=0)
-    self.name = name
-    self.alertness = alertness
-  end
     def has_coffee?
-    self.coffee.to_i > 0
-  end
-  def needs_coffee?
-    self.coffee.to_i == 0
-  end
-  def buy (x)
-    self.coffee = x
-  end
-  def drink!
-    self.alertness += 0.33
-    self.coffee.sip!
+        coffee.to_i > 0
   end
 
+    def needs_coffee?
+        coffee.to_i.zero?
+    end
 
+    def buy(x)
+        self.coffee = x
+    end
 
+    def drink!
+        self.alertness += 0.33
+        coffee.sip!
+    end
 end
